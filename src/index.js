7b123835fd15dc5ec635b6e8c1d711ee33a0056c
Julia Pritychenko <juliasnoww@mail.ru>
2018-10-06 17:45:10 +0300

    longest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let count = 1;
+    let temp = [];
+    for (let i = 0; i < array.length; i++){
+        if (array[i]===array[i+1]-1){
+            count++;
+        } if (array[i]!== array[i+1]-1 ) {
+            if (count!== 1) {temp.push(count)};
+            count = 1;
+        }
+    }
+    if (array.length === 0 ) return 0;
+    else if(Math.max.apply(null,temp) === -Infinity ) return 1;
+    else return Math.max.apply(null,temp);
+};
